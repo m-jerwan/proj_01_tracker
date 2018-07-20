364c89a228f57a0ed022e48c6149ed3178018897
Marcin Jerwan <m.jerwan@gmail.com>
2018-07-20 16:12:27 +0100

    creating empty tables

---
+++ db/tracker.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS merchants;
+DROP TABLE IF EXISTS tags;
+
+CREATE TABLE merchants(
+  id SERIAL8 primary key,
+  merchant_name VARCHAR(255)
+);
+
+CREATE TABLE tags(
+  id SERIAL8 primary key,
+  tag_name VARCHAR(255)
+);
+
+CREATE TABLE transactions(
+  id SERIAL8 primary key,
+  merchant_id INT8 references merchants(id),
+  tag_id INT8 references tags(id)
+)
