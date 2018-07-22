5a8795d4977dba07c04e2978320272eba72dabe7
Marcin Jerwan <m.jerwan@gmail.com>
2018-07-22 15:44:35 +0100

    view structure start

--- tracker_controller.rb
+++ tracker_controller.rb
@@ -1,6 +1,10 @@
 require('sinatra')
 require('sinatra/contrib/all') if development?
 
+require_relative('controllers/merchants_controller')
+require_relative('controllers/tags_controller')
+require_relative('controllers/transactions_controller')
+
 get '/' do
-  "tracking...."
+  erb (:index)
 end
