47d257b87c57320d084dc5ea055ce3717ffc8c68
Marcin Jerwan <m.jerwan@gmail.com>
2018-07-25 11:06:02 +0100

    tags REST done

--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -4,3 +4,37 @@ get '/tags' do
   @tags = Tag.all()
   erb (:"tags/index")
 end
+
+#NEW
+get "/tags/new" do
+  erb (:"tags/new")
+end # &CREATE
+post "/tags" do
+  @tag = Tag.new(params)
+  @tag.save
+  erb (:"tags/create")
+end
+
+#SHOW
+get '/tags/:id' do
+  @tag = Tag.find(params['id'])
+  erb (:"tags/show")
+end
+
+#edit
+post "/tags/:id/edit" do
+  @tag = Tag.find(params['id'])
+  erb (:"tags/edit")
+end # &UPDATE
+post "/tags/:id" do
+  tag = Tag.new(params)
+  tag.update
+  redirect to "tags/#{params['id']}"
+end
+
+#delete
+post "/tags/:id/delete" do
+  tag = Tag.find(params['id'])
+  tag.delete
+  redirect to "tags"
+end
