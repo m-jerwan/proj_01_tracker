ba59663e0d416d23f93bc224b6968e3b14f54530
Marcin Jerwan <m.jerwan@gmail.com>
2018-07-26 15:49:06 +0100

    adding class Limit. ERRORS

---
+++ models/limit.rb
@@ -0,0 +1,32 @@
+require_relative('../db/sqlRunner.rb')
+require_relative('merchant.rb')
+require_relative('tag.rb')
+require_relative('transaction.rb')
+require( 'pry-byebug' )
+require 'date'
+
+
+class  Limit
+
+def initialize
+  @now = DateTime.now
+  @merchants = Merchant.all()
+  @transactions = Transaction.all()
+  @tags = Tag.all()
+end
+
+
+def self.monthly_spending_for(filter)
+@monthly_spending_for = 0
+  for transaction in @transactions
+    if transaction.find_merchant.merchant_name == filter || transaction.find_tag.tag_name == filter
+      if transaction.time_stamp.strftime("%-m") == @now.strftime("%-m")
+        @monthly_spending_for += transaction.amount
+      end
+    end
+  end
+  return @monthly_spending_for
+end
+
+
+end
--- tracker_controller.rb
+++ tracker_controller.rb
@@ -5,9 +5,12 @@ require('sinatra/contrib/all') if development?
 require_relative('controllers/merchants_controller.rb')
 require_relative('controllers/tags_controller')
 require_relative('controllers/transactions_controller')
+require_relative('models/limit')
 
 get '/' do
+  @now = DateTime.now
+  @merchants = Merchant.all()
+  @transactions = Transaction.all()
+  @tags = Tag.all()
   erb (:index)
-
-  #for main index page/stats/alerts
 end
